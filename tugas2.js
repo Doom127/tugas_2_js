33020286560978824cca15da45a5bb79efac2d5d
Muhamad Syamil <syamil.r@gmail.com>
2020-11-24 05:39:12 +0700

    Tugas 2 Javascript
    Tugas dikumpulkan untuk NIOMIC

---
+++ tugas2.js
@@ -0,0 +1,21 @@
+var number = 99;
+var string = 'syamil';
+var boolean1= 100;
+var boolean2 = 200;
+
+if (boolean1!=boolean2)
+hasil = true;
+else {
+hasil = false;
+}
+
+var tipe_null = "tester";
+tipe_null=null;
+
+var array = [1,2,3,4,5,6,7,8,9];
+
+console.log(number);
+console.log(string);
+console.log(hasil);
+console.log(tipe_null);
+console.log(array[7]);
